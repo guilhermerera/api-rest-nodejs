839d659effe5572f7fbd77eb6452c4f1855d9ed1
Guilherme Rera <guilhermerera@gmail.com>
2023-06-21 22:32:42 -0300

    added test database

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,18 +1,27 @@
-import "dotenv/config";
+import { config } from "dotenv";
 import { z } from "zod";
 
+if (process.env.NODE_ENV === "test") {
+    config({ path: ".env.test" });
+} else {
+    config();
+}
+
 const envSchema = z.object({
-    ENVIRONMENT: z.enum(["development", "test", "production"]).default("production"),
+	ENVIRONMENT: z
+		.enum(["development", "test", "production"])
+		.default("production"),
 	DATABASE_URL: z.string(),
-	PORT: z.number().default(3333),
+	PORT: z.number().default(3333)
 });
 
 const _env = envSchema.safeParse(process.env);
 
 if (!_env.success) {
-    console.error("⛔️ Invalid environment variables:", _env.error.format());
-    throw new Error("Invalid environment variables. Please see the logs above for more details.");
+	console.error("⛔️ Invalid environment variables:", _env.error.format());
+	throw new Error(
+		"Invalid environment variables. Please see the logs above for more details."
+	);
 }
 
 export const env = _env.data;
-
--- test/transactions.test.ts
+++ test/transactions.test.ts
@@ -1,16 +1,26 @@
-import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { afterAll, afterEach, beforeAll, beforeEach, describe, expect, it } from "vitest";
+import { execSync } from "node:child_process";
 import request from "supertest";
 import { app } from "../src/app";
 
 describe("Transactions routes", () => {
 	beforeAll(async () => {
 		await app.ready();
+
 	});
 
 	afterAll(async () => {
 		await app.close();
 	});
 
+	beforeEach(async () => {
+		execSync("npm run knex migrate:latest");
+	})
+
+	afterEach(async () => {
+		execSync("npm run knex migrate:rollback --all");
+	})
+	
 	it("should be able to create a new transaction", async () => {
 		await request(app.server)
 			.post("/transactions")
