1d48eeea11b4e392a942d134c8c1f0c1b5b7b2dc
Guilherme Rera <guilhermerera@gmail.com>
2023-06-22 18:25:33 -0300

    fixed typo on migration

--- db/migrations/20230607213828_create-transactions.ts
+++ db/migrations/20230607213828_create-transactions.ts
@@ -12,6 +12,6 @@ export async function up(knex: Knex): Promise<void> {
 
 
 export async function down(knex: Knex): Promise<void> {
-    return await knex.schema.dropTable("trasactions");
+    return await knex.schema.dropTable("transactions");
 }
 
